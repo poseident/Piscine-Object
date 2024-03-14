8013e2240847142e0b07c3f308d41e411488e8ef
Benjamin Schweitzer <benjamin.schweitzer1701@gmail.com>
2024-03-14 02:36:29 +0100

    ex01

---
+++ module 00/ex01/includes/Vector2.hpp
@@ -0,0 +1,16 @@
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
+struct Vector2 
+{
+    float X;
+    float Y;
+    
+    Vector2(float x, float y) : X(x), Y(y) {}
+
+    bool operator==(const Vector2& other) const {
+        return X == other.X && Y == other.Y;
+    }
+};
+
+#endif
---
+++ module 00/ex01/srcs/Graph.cpp
@@ -0,0 +1,69 @@
+#include "../includes/Graph.hpp"
+
+Graph::Graph(float X, float Y) : size(X, Y) {}
+
+Graph::~Graph()
+{
+    ;
+}
+
+void Graph::print_graph()
+{
+    float maxX = size.X;
+    float maxY = size.Y;
+
+        // Output the graph representation
+        for (float y = maxY - 1; y >= 0; --y) {
+            std::cout << std::setw(2) << y << " "; // Print y-coordinate
+            for (float x = 0; x < maxX; ++x) {
+                bool pointFound = false;
+                for (size_t i = 0; i < points.size(); ++i) {
+                if (points[i].X == x && points[i].Y == y) {
+                    pointFound = true;
+                    break;
+                }
+            }
+            if (pointFound) {
+                std::cout << "X ";
+            } else {
+                std::cout << ". ";
+            }
+        }
+        std::cout << std::endl;
+        }
+
+        // Output the x-axis labels
+        std::cout << "  ";
+        for (int x = 0; x < maxX; ++x) {
+            std::cout << std::setw(2) << x; // Print x-coordinate
+        }
+        std::cout << std::endl;
+}
+
+void Graph::remove_point(float X, float Y)
+{
+    for (size_t i = 0; i < this->points.size(); i++) {
+        if (this->points[i].X == X && this->points[i].Y == Y)
+        {
+            this->points.erase(this->points.begin() + i);
+            std::cout << "point sucessfully removed" << std::endl;
+            return ;
+        }
+    }
+    std::cout << "parameter don't match any point in the graph" << std::endl;
+    return ;
+}
+
+void Graph::add_point(float X, float Y)
+{
+    std::cout << this->size.Y << "param " << Y << std::endl; 
+    if (X < this->size.X && Y < this->size.Y)
+    {
+        Vector2 new_point(X, Y);
+        this->points.push_back(new_point);
+        std::cout << "point sucessfully added" << std::endl;
+        return ;
+    }
+    std::cout << "parameter don't match graph size" << std::endl;
+    return ;
+}
