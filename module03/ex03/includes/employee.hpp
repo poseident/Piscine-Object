c553278987c1b6c7beab1519f07201f5042110b3
poseident <benjamin.schweitzer1701@gmail.com>
2024-04-22 16:16:17 +0200

    ok

--- module03/ex03/includes/employee.hpp
+++ module03/ex03/includes/employee.hpp
@@ -4,9 +4,14 @@
 class Employee
 {
     public:
+        Employee(const int id, const int hourlyValue): _id(id), _hourlyValue(hourlyValue) {}
         int executeWorkday();
+        const int getId() {
+            return _id;
+        }
     private:
-        int hourlyValue;
+        const int _id;
+        int _hourlyValue;
 };
 
 #endif
--- module03/ex03/includes/employeeManagement.hpp
+++ module03/ex03/includes/employeeManagement.hpp
@@ -2,15 +2,49 @@
 #define EMPLOYEEMANAGEMENT_HPP
 
 #include "employee.hpp"
+#include <map>
+#include <iostream>
 
 class EmployeeManager
 {
     public:
-        void addEmployee(Employee*);
-        void removeEmployee(Employee*);
+        EmployeeManager() : _nextId(1) {}
+        ~EmployeeManager(){
+            for (std::map<int, Employee*>::iterator it = _employees.begin(); it != _employees.end(); ++it)
+            {
+                delete it->second;
+            }
+        }
+        void addEmployee(Employee* addEmployee){
+            _employees[_nextId] = addEmployee;
+            _nextId++;
+        }
+        void removeEmployee(Employee* employee){
+            std::map<int, Employee*>::const_iterator it = _employees.find(employee->getId());
+            if (it != _employees.end()){
+                delete it->second;
+                _employees.erase(it);
+            }
+            else
+                throw std::runtime_error("Employee not found");
+        }
+        Employee& createEmployee(int hourly_value){
+            Employee* new_employee = new Employee(_nextId, hourly_value);
+           addEmployee(new_employee);
+            return *new_employee;
+        }
+        const Employee& getEmployeeById(int id) const {
+            std::map<int, Employee*>::const_iterator it = _employees.find(id);
+            if (it != _employees.end())
+                return *(it->second);
+            else
+                throw std::runtime_error("Employee not found");
+        }
         void executeWorkday();
         void calculatePayroll();
     private:
+        int _nextId;
+        std::map<int, Employee*> _employees;
 };
 
 #endif
