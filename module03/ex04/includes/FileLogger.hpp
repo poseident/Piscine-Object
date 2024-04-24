5e6b56d8056983618a232fa748c1c5d2db3cc515
Benjamin Schweitzer <benjamin.schweitzer1701@gmail.com>
2024-04-24 15:12:26 +0200

    ok

---
+++ module03/ex04/includes/FileLogger.hpp
@@ -0,0 +1,10 @@
+#ifndef FILELOGGER_HPP
+#define FILELOGGER_HPP
+
+class FileLogger
+{
+    public:
+    private:
+};
+
+#endif
---
+++ module03/ex04/includes/OstreamLogger.hpp
@@ -0,0 +1,10 @@
+#ifndef OSTREAMLOGGER_HPP
+#define OSTREAMLOGGER_HPP
+
+class OstreamLogger
+{
+    public:
+    private:
+};
+
+#endif
